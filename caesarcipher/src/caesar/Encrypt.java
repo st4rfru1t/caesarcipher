5867af5eb77402ffc933a3ca02d05ae9f6683a01
Heeral Dulobdas <zkac317@live.rhul.ac.uk>
2024-06-11 02:05:44 +0100

    updated encrypt()

--- caesarcipher/src/caesar/Encrypt.java
+++ caesarcipher/src/caesar/Encrypt.java
@@ -1,12 +1,23 @@
 package caesar;
 
 public class Encrypt {
+
+  public String encrypt(String msg, int shift) {
+    
+    String encoded = ""; // empty string we build by iterating
+    for (int i = 0; i < msg.length(); i++) {
+
+      // get single char at index i, then turn it into ASCII value
+      char tempChar = msg.charAt(i);
+      int ascii = (int) tempChar;
+      // increment each letter by the specified shift
+      ascii += shift;
+      tempChar = (char) ascii;
+      // append to end string
+      encoded += tempChar;
   
-  public String encrypt(String msg) {
-    for (int i = 0; i<msg.length(); i++) {
-      
     }
-    return null;
+    return encoded;
   }
-  
+
 }
