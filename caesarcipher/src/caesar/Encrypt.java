3025520f9c2cccda412e7c346bb854eb97562775
Heeral Dulobdas <zkac317@live.rhul.ac.uk>
2024-06-12 23:39:35 +0100

    kept ascii values between 65-90

--- caesarcipher/src/caesar/Encrypt.java
+++ caesarcipher/src/caesar/Encrypt.java
@@ -5,14 +5,19 @@ public class Encrypt {
   public String encrypt(String msg, int shift) {
     
     String encoded = ""; // empty string we build by iterating
+    msg = msg.toUpperCase();
     for (int i = 0; i < msg.length(); i++) {
-
       // get single char at index i, then turn it into ASCII value
       char tempChar = msg.charAt(i);
       int ascii = (int) tempChar;
-      // increment each letter by the specified shift
-      ascii += shift;
-      tempChar = (char) ascii;
+      // increment each letter by the specified shift 65-90.
+      if (ascii + shift > 90) { // 89
+        int overflow = (ascii + shift) - 90; // 89 + 2 = 91 - 90 = 1
+        ascii = (char) overflow;
+      } else {
+        ascii += shift;
+      }
+      ascii = (char) ascii;
       // append to end string
       encoded += tempChar;
   
