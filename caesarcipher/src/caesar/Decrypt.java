22b5f74f58968e70a0ab1fe0719f57fd81740892
Heeral Dulobdas <zkac317@live.rhul.ac.uk>
2024-06-13 00:20:26 +0100

    updated main menu

--- caesarcipher/src/caesar/Decrypt.java
+++ caesarcipher/src/caesar/Decrypt.java
@@ -6,10 +6,9 @@ public class Decrypt {
     
   }
 
-  public static void Decrypt(String msg) {
+  public static void decrypt(String msg) {
     for (int i = 0; i < 24; i++) { // try 24 times until correct combo chosen
       System.out.println(Encrypt.encrypt(msg, i));
     }
   }
-  
 }
