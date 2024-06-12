e16f6ed2192942cec522d342a9c97a09fdaf312c
Heeral Dulobdas <zkac317@live.rhul.ac.uk>
2024-06-13 00:45:43 +0100

    removed 0 shift in decrypt(), unnecessary to have a shift of 0

--- caesarcipher/src/caesar/Decrypt.java
+++ caesarcipher/src/caesar/Decrypt.java
@@ -7,7 +7,7 @@ public class Decrypt {
   }
 
   public static void decrypt(String msg) {
-    for (int i = 0; i < 24; i++) { // try 24 times until correct combo chosen
+    for (int i = 1; i < 24; i++) { // try 24 times until correct combo chosen
       System.out.println(Encrypt.encrypt(msg, i));
     }
   }
